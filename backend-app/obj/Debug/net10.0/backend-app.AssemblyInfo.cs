83b8f7192c96f60cd2a01f4ec2d8d0e9c0eaae41
Josué Junior <josuej@ciandt.com>
2025-12-24 16:20:55 -0300

    ok

--- backend-app/obj/Debug/net10.0/backend-app.AssemblyInfo.cs
+++ backend-app/obj/Debug/net10.0/backend-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59cce2e50636fef8a2b7258b658eae19fe49aea1")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
